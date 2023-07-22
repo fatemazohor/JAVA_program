bf1e1619c31f8592d686f39cf3956d127fde0254
Fatema Tuz Zohora <fatematuzzohora289@gmail.com>
2023-07-22 13:05:56 +0600

    update_22/07/23

--- GuessNumber/src/guessnumber/GuessNumber.java
+++ GuessNumber/src/guessnumber/GuessNumber.java
@@ -19,21 +19,86 @@ public class GuessNumber {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         
-        int firstNumber = (int)(Math.random()*10); 
+//        int firstNumber = (int)(Math.random()*10); 
+//                    
+//                    for(int i = 1; i <=5; i++){
+//                    System.out.println("Guess the number");
+//                    int answer = input.nextInt();
+//                    
+//                    
+//                    if(answer >= 10){
+//                        System.out.println("Only one Digit number is allowed");    
+//                    }
+//                    else if(firstNumber == answer){
+//                        System.out.println("Hurray! you win " +"and the number is " + firstNumber);
+//                        break;
+//                    }
+//                    else{
+//                        System.out.println("Better luck next time ");
+//                    }                    
+//                    }                    
+                    
+                    
+                   /**
+                    * Guess a number between 20 to 39.
+                    */
+                    int guess = (20 + (int)(Math.random()*20));
                     
                     for(int i = 1; i <=5; i++){
-                    System.out.println("Guess the number");
-                    int answer = input.nextInt();
+                    System.out.println("Guess the number between 20 to 39");
+                    int yourNumber = input.nextInt();
                     
-                    if(firstNumber == answer){
-                        System.out.println("Hurray! you win " + firstNumber);
-                        break;
+//                    guess number 2nd digit
+                    int number1 = guess/10;
+//                    guess number 1st digit
+                    int number2 = guess%10;
+                    
+//                    answer number 2nd digit
+                    int digit1 = yourNumber/10;
+//                    answer number 1st digit.
+                    int digit2 = yourNumber%10;
+                    
+//                    only 2 digit number is 
+                    if(yourNumber >= 100){
+                        System.out.println("Only two Digit number is allowed");
+                        
                     }
                     else{
-                        System.out.println("Better luck next time ");
+                        
+                        if(guess == yourNumber){
+                        System.out.println("Hurray! complete match. you won 1000 taka." );
+                        break;
+                        }
+                        else if(number1 == digit1 || number2 == digit2){
+                            System.out.println("You won 500 taka for one digit match.");
+                            break;
+                        }
+                        else if(
+                                number1 == digit1 
+                                || number1 == digit2
+                                || number2 == digit1 
+                                || number2 == digit2
+                                ){
+                            System.out.println("You only won 200 taka.");
+                            break;
+                        }
+                        
+                        else{
+                            System.out.println("Better luck next time ");
+                        }
+                        
+                    }
                     }
                     
-                    }
+                    
+                    
+                    
+                    
+                    
+                    
+                    
+                    
+                   
     }
     
 }
