87631717c0bbac5f9456e2c22092523a8ec795d1
Fatema Tuz Zohora <fatematuzzohora289@gmail.com>
2023-08-12 23:52:57 +0600

    update

--- TestFile/src/testfile/ReadDataWeb.java
+++ TestFile/src/testfile/ReadDataWeb.java
@@ -14,45 +14,68 @@ public class ReadDataWeb {
 
     public static void main(String[] args) throws FileNotFoundException {
         //create file object
-//        File myFile = new File("C:\\Users\\user\\Desktop\\TenLineText.txt");
-//        //check if file exist
-//        myFile.exists();
-        File mydoc = new File("C:\\Users\\user\\Desktop\\newdoc.docx");
-        System.out.println(mydoc.exists());
-        PrintWriter myd = new PrintWriter(mydoc);
-        System.out.println(mydoc.exists());
-        myd.print("hello");
-        myd.close();
-//        PrintWriter input = new PrintWriter(myFile);
-//        //enter text in file
-//        input.print("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum");
-//       //close file 
-//        input.close();
-//           //check if file exist
-//        System.out.println(myFile.exists());
-//
-//        Scanner output = new Scanner(myFile);
-//        int count = 0;
-//        String next = "";
-//        int i = 1;
-//        while (output.hasNext()) {
-//            //read file and store 10 word in next Variable
-//            next += output.next() + " ";
-//
-//            count++;
-//            if (count == 10) {
-//                //print 10 word
-//                System.out.println(" line " + i + " " + next);
-//                //restart count word
-//                next = "";
-//                count = 0;
-//                //count line 
-//                i++;
-//            }
-//
-//        }
-//        //close file
-//        output.close();
+        File myFile = new File("C:\\Users\\joty_\\Desktop\\TenLineText.txt");
+        //check if file exist
+        System.out.println(myFile.exists());
+
+        PrintWriter input = new PrintWriter(myFile);
+        //enter text in file
+        input.print("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum");
+        //close file 
+        input.close();
+        //check if file exist
+        System.out.println(myFile.exists());
+        //print first 10 word per line
+        Scanner output = new Scanner(myFile);
+        int count = 0;
+        String next = "";
+        int i = 1;
+        while (output.hasNext()) {
+            //read file and store 10 word in next Variable
+            next += output.next() + " ";
+
+            count++;
+            if (count == 10) {
+                //print 10 word
+                System.out.println(" line " + i + " " + next);
+                //restart count word
+                next = "";
+                count = 0;
+                //count line 
+                i++;
+            }
+
+        }
+        //close file
+        output.close();
+        //print first 10 word per line if match condition
+        int countN = 0;
+        String nextN = "";
+        String matchText = "";
+        String sourceText = "dolor";
+        int l = 1;
+        while (output.hasNext()) {
+            //read file and store 10 word in next Variable
+            matchText = output.next();
+
+            if (matchText.matches(sourceText)) {
+
+                for (int j = 0; j < 10; j++) {
+                    nextN += output.next() + " ";
+                    countN++;
+                }
+                System.out.println("line " + l + " " + nextN);
+                //restart count word
+                nextN = "";
+                countN = 0;
+                //count line 
+                l++;
+
+            }
+
+        }
+        //close file
+        output.close();
 
     }
 
---
+++ TestFile/src/testfile/WebCrawl.java
@@ -0,0 +1,62 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package testfile;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.Scanner;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author joty_
+ */
+public class WebCrawl {
+
+    public static void main(String[] args) throws IOException {
+
+        try {
+            URL url;
+            url = new URL("https://en.wikipedia.org/wiki/Web_crawler");
+            Scanner input = new Scanner(url.openStream());
+            String matchT = "";
+            String sourceT = "Web crawler";
+            int j = 0;
+            String next = "";
+            while(j <= 10) {
+
+                while (input.hasNext()) {
+                    matchT = input.next();
+                    if (matchT.matches(sourceT)) {
+                        for (int i = 0; i < 10; i++) {
+                            next += input.next() + " ";
+                        }
+                        System.out.println("LINE: " + next);
+                        next = "";
+
+                    } j++;
+
+                }
+                //can't figure how to stop file excution
+
+               
+            }
+            File file = new File("C:\\Users\\joty_\\Desktop\\web.text");
+            PrintWriter output = new PrintWriter(file);
+            System.out.println(file.exists());
+            System.out.println(file);
+
+        } catch (MalformedURLException ex) {
+            Logger.getLogger(WebCrawl.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+    }
+
+}
