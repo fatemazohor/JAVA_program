2c7f46515ef6dff1d7e25a276018256e96322f9a
Fatema Tuz Zohora <fatematuzzohora289@gmail.com>
2023-08-30 20:53:39 +0600

    up

---
+++ VehicleManagementSystem/src/edible/Apple.java
@@ -0,0 +1,21 @@
+package edible;
+
+public class Apple extends Fruits{
+
+    public Apple() {
+    }
+
+    public Apple(double unitPrice, int unitOfProduct) {
+        super(unitPrice, unitOfProduct);
+    }
+
+    @Override
+    public double totalPrice() {
+        return super.totalPrice(); 
+    }
+
+    
+    
+    
+    
+}
---
+++ VehicleManagementSystem/src/interfacepak/TaxableInterface.java
@@ -0,0 +1,8 @@
+package interfacepak;
+
+public interface TaxableInterface {
+    double taxAmount = 0.02;
+    
+    public abstract double priceAfterTax();
+    
+}
