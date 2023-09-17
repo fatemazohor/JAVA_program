c478d0ceb005e5c55cf616ab1e1b468178600004
Fatema Tuz Zohora <fatematuzzohora289@gmail.com>
2023-09-17 12:47:18 +0600

    add17-09-23

---
+++ FinalEvidence5/src/finalevidence5/FinalEvidence5.java
@@ -0,0 +1,18 @@
+package finalevidence5;
+
+import view.EmailValidation;
+
+public class FinalEvidence5 {
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+        EmailValidation ev = new EmailValidation();
+        ev.setTitle("Email Validation");
+        ev.setLocationRelativeTo(null);
+        ev.setVisible(true);
+        
+        
+        
+    }
+    
+}
---
+++ Finalevd5.2/src/finalevd5/pkg2/Finalevd52.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package finalevd5.pkg2;
+
+import view.From;
+
+
+public class Finalevd52 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        From from = new From();
+        from.setTitle("Data Entry From");
+        from.setLocationRelativeTo(null);
+        from.setVisible(true);
+        
+    }
+    
+}
